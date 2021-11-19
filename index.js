8c5acfc063b6dc6b7d7455dfc72ef16d1863059c
heyset <c.inaciomatheus@gmail.com>
2021-11-19 18:29:33 -0300

    Modifies start script and adds 404 handling

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 
 const port = process.env.PORT || 3030;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+const distPath = path.join(__dirname, 'dist');
+
+app.use(express.static(distPath));
+
+app.get('*', (_req, res) => res.status(200).sendFile(path.join(distPath, 'index.html')));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
